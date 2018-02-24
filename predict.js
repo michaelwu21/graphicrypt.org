9dedcaccb807dd642b8c56fe4eeb1d7325c78419
MatthewChen37 <epicninjasauce@gmail.com>
2018-02-24 17:06:08 -0500

    Update predict.js

--- predict.js
+++ predict.js
@@ -1,9 +1,4 @@
 function predict(link) {
-	/*$.get("https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=10", function(data, status) {
-		for (var i = 0; i < data.length - 1; i++) {
-			console.log(data[i]);
-	  }
-	})*/
 	prices = [];
 	$.getJSON(link, function(data) {
 		for(var i = 0; i < data.Data.length; i++) {
