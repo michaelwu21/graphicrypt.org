6fb9da8013dfcb3ffbb8e1a21aa2fc45fd03e13c
michael wu <michaelwu21@gmail.com>
2018-02-24 17:45:31 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -5,10 +5,24 @@ $(function() {
 	      $("#price").html(data[i].price_usd);
 	    }
 	  }
+	});
+	$("body").click(function () {
+		if($("#searchCoin").is(":focus")) {
+			$("#sign_in_button").hide();
+			$("#searchCoin").addClass("expand");
+		} else {
+			$("#searchCoin").removeClass("expand");
+			$("#sign_in_button").show();
+		}
+	})
+	$("#sign_in_button").click(function() {
+		$("#mainpage").hide();
+		$("#sign_in").fadeIn(500);
+	})
+	$(".close_sign").click(function() {
+		$("#sign_in").hide();
+		$("#sign_up").hide();
+		$("#mainpage").fadeIn(500);
+	})
 });
 
-
-
-
-
-});
